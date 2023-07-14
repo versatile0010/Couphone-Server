cffa39dbe27f1467f3e1dd4d16e30e80cc5c60a5
versatile0010 <siam0294@naver.com>
2023-07-14 22:16:27 +0900

    initial commit

---
+++ src/main/java/com/example/couphoneserver/CouphoneServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.couphoneserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouphoneServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouphoneServerApplication.class, args);
+	}
+
+}
