25810e389ab16fca91a4ece221e2e7d7809bde9b
limsubinn <mymyx02@naver.com>
2023-07-26 22:33:15 +0900

    docs: BrandController 태그 추가

--- src/main/java/com/example/couphoneserver/controller/BrandController.java
+++ src/main/java/com/example/couphoneserver/controller/BrandController.java
@@ -6,6 +6,7 @@ import com.example.couphoneserver.dto.brand.PostBrandRequest;
 import com.example.couphoneserver.dto.brand.PostBrandResponse;
 import com.example.couphoneserver.service.BrandService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "brands", description = "브랜드 관련 API")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
