48a85d29d2e9042ccb4390383c38d6e237370701
versatile0010 <siam0294@naver.com>
2023-07-27 18:05:13 +0900

    feat: swagger tag 추가

--- src/main/java/com/example/couphoneserver/controller/MemberController.java
+++ src/main/java/com/example/couphoneserver/controller/MemberController.java
@@ -6,11 +6,16 @@ import com.example.couphoneserver.dto.member.AddMemberRequest;
 import com.example.couphoneserver.dto.member.AddMemberResponse;
 import com.example.couphoneserver.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "회원 컨트롤러", description = "회원 관련 API 입니다.")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -21,7 +26,7 @@ public class MemberController {
     @NoAuth
     @PostMapping
     @Operation(summary = "휴대폰 번호로 회원 가입", description = "request = {이름, 휴대폰 번호, 비밀번호}")
-    public BaseResponse<AddMemberResponse> signup(@Valid @RequestBody AddMemberRequest request){
+    public BaseResponse<AddMemberResponse> signup(@Valid @RequestBody AddMemberRequest request) {
         return new BaseResponse<>(memberService.save(request));
     }
 
