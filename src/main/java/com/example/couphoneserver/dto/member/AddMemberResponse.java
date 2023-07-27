22044d858b2fb38a41750c956e18a7fbb15b040a
versatile0010 <siam0294@naver.com>
2023-07-27 17:49:03 +0900

    refactor: post /users response body 변경 - 기존: id, name, phoneNumber, memberGrade - 변경 후: id

--- src/main/java/com/example/couphoneserver/dto/member/AddMemberResponse.java
+++ src/main/java/com/example/couphoneserver/dto/member/AddMemberResponse.java
@@ -1,6 +1,5 @@
 package com.example.couphoneserver.dto.member;
 
-import com.example.couphoneserver.domain.MemberGrade;
 import com.example.couphoneserver.domain.entity.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
@@ -14,17 +13,7 @@ import lombok.NoArgsConstructor;
 public class AddMemberResponse {
     @Schema(example = "1", description = "회원 아이디")
     private Long id;
-    @Schema(example = "김이름", description = "회원 이름")
-    private String name;
-    @Schema(example = "010-1234-5678", description = "휴대폰 번호")
-    private String phoneNumber;
-    @Schema(example = "ROLE_MEMBER", description = "회원 권한")
-    private MemberGrade memberGrade;
-
     public AddMemberResponse(Member member) {
         id = member.getId();
-        name = member.getName();
-        phoneNumber = member.getPhoneNumber();
-        memberGrade = member.getGrade();
     }
 }
