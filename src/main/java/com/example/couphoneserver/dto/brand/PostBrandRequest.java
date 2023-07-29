699ee451ed8a3d20680636e8ef2b260de81596ea
limsubinn <mymyx02@naver.com>
2023-07-29 18:31:06 +0900

    docs: swagger 브랜드 등록 API 스키마 수정

--- src/main/java/com/example/couphoneserver/dto/brand/PostBrandRequest.java
+++ src/main/java/com/example/couphoneserver/dto/brand/PostBrandRequest.java
@@ -2,10 +2,8 @@ package com.example.couphoneserver.dto.brand;
 
 import com.example.couphoneserver.domain.entity.Brand;
 import com.example.couphoneserver.domain.entity.Category;
-import com.example.couphoneserver.domain.entity.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -14,16 +12,18 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostBrandRequest {
-    @Schema(example = "메가커피")
+    @Schema(description = "브랜드 이름", example = "메가커피")
     @NotNull
     private String name;
-    @Schema(example = "아이스 아메리카노 1잔 무료")
+    @Schema(description = "보상 방법 (쿠폰 혜택)", example = "아이스 아메리카노 1잔 무료")
     @NotNull
     private String rewardDescription;
-    @Schema(example = "----")
+    @Schema(description = "브랜드 로고 URL", example = "----")
+    @NotNull
     private String brandImageUrl;
 
-    @Schema(example = "1")
+    @Schema(description = "카테고리 ID", example = "1")
+    @NotNull
     private Long categoryId;
 
     public Brand toEntity(Category category) {
