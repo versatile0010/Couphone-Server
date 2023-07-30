5fa372b3a5e150bd845e77971616230240026d6d
limsubinn <mymyx02@naver.com>
2023-07-30 21:53:34 +0900

    feat: 쿠폰 생성 dto

---
+++ src/main/java/com/example/couphoneserver/dto/coupon/PostCouponResponse.java
@@ -0,0 +1,17 @@
+package com.example.couphoneserver.dto.coupon;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostCouponResponse {
+
+    @Schema(description = "쿠폰 ID", example = "1")
+    Long couponId;
+
+    @Schema(description = "쿠폰 생성 날짜", example = "2023-07-29 18:35:46.434060")
+    LocalDateTime createdDate;
+
+}
