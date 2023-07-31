1884f5d90e5fdf975538a5584b563968a92ff024
akfrdma0125 <nogain1215@gmail.com>
2023-07-31 23:53:37 +0900

    feat: 카테고리 조회 기능 추가
    카테고리 조회 기능을 구현했습니다.
    모든 카테고리 혹은 원하는 이름의 카테고리를 조회할 수 있습니다.

--- src/main/java/com/example/couphoneserver/repository/CategoryRepository.java
+++ src/main/java/com/example/couphoneserver/repository/CategoryRepository.java
@@ -1,10 +1,15 @@
 package com.example.couphoneserver.repository;
 
 import com.example.couphoneserver.domain.entity.Category;
+import com.example.couphoneserver.repository.mapping.CategoryInfoMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findById(Long id);
+
+    List<Category> findByNameLike(String query);
 }
---
+++ src/main/java/com/example/couphoneserver/repository/mapping/CategoryInfoMapping.java
@@ -0,0 +1,6 @@
+package com.example.couphoneserver.repository.mapping;
+
+public interface CategoryInfoMapping {
+    Long getId();
+    String getName();
+}
