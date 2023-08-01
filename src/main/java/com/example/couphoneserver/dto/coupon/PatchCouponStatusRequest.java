c3d83b65c4a02b0da3c4610e55ea71f853441289
limsubinn <mymyx02@naver.com>
2023-08-01 09:21:12 +0900

    feat: 쿠폰 사용 dto 생성
    PatchCouponStatusRequest, PatchCouponStatusResponse

---
+++ src/main/java/com/example/couphoneserver/dto/coupon/PatchCouponStatusRequest.java
@@ -0,0 +1,19 @@
+package com.example.couphoneserver.dto.coupon;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatchCouponStatusRequest {
+
+    @Schema(description = "멤버 ID", example = "1")
+    Long memberId;
+
+    @Schema(description = "브랜드 ID", example = "1")
+    Long brandId;
+
+}
