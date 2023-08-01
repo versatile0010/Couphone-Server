cbea77c2b3c768472a5cb541cdb7cdfde6ebfa82
limsubinn <mymyx02@naver.com>
2023-08-01 10:57:49 +0900

    feat: 브랜드 상세 조회 로직 작성

--- src/main/java/com/example/couphoneserver/repository/CouponItemRepository.java
+++ src/main/java/com/example/couphoneserver/repository/CouponItemRepository.java
@@ -4,7 +4,10 @@ import com.example.couphoneserver.domain.CouponItemStatus;
 import com.example.couphoneserver.domain.entity.CouponItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CouponItemRepository extends JpaRepository<CouponItem, Long> {
     CouponItem findByMemberIdAndBrandIdAndStatus(Long mid, Long bid, CouponItemStatus status);
     CouponItem findByMemberIdAndBrandId(Long mid, Long bid);
+    List<CouponItem> findAllByMemberIdAndBrandId(Long mid, Long bid);
 }
