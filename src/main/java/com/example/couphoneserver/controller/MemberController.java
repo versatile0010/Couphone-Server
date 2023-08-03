6c0ed01bd1e62e7b6a21086f7ebfae0e05de8840
versatile0010 <siam0294@naver.com>
2023-08-03 16:12:02 +0900

    feat: 회원이 소유한 쿠폰의 브랜드 리스트 조회 API - Controller 계층 개발

--- src/main/java/com/example/couphoneserver/controller/MemberController.java
+++ src/main/java/com/example/couphoneserver/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.example.couphoneserver.controller;
 
 import com.example.couphoneserver.common.response.BaseResponse;
 import com.example.couphoneserver.domain.entity.Member;
+import com.example.couphoneserver.dto.member.response.GetMemberCouponBrandsResponse;
 import com.example.couphoneserver.dto.member.response.GetMemberResponse;
 import com.example.couphoneserver.dto.member.response.PatchMemberResponse;
 import com.example.couphoneserver.service.MemberService;
@@ -33,5 +34,20 @@ public class MemberController {
     public BaseResponse<GetMemberResponse> show(@PathVariable("member-id") Long memberId) {
         Member member = memberService.findOneById(memberId);
         return new BaseResponse<>(memberService.getMemberInfo(member));
+    }
+
+    @GetMapping("/{member-id}/brands")
+    @Operation(summary = "정렬 조건에 따른 브랜드 조회", description =
+            """
+                    path variable 으로 member-id 를 보내면 해당 회원이 가지고 있는 쿠폰 브랜드 리스트를 반환합니다.
+                    정렬 조건은 query string 으로 sort 값을 보내주세요. {1, 2, 3} 에 따라 달라집니다.
+                    - 1(default)번 옵션은 쿠폰 많은 순, 생성 시간이 이른 순
+                    - 2번 옵션은 생성 시간이 이른 순, 쿠폰 많은 순
+                    - 3번 옵션은 브랜드 이름 순으로 정렬하여 데이터를 반환합니다.
+                    """)
+    public BaseResponse<GetMemberCouponBrandsResponse> getBrands(
+            @PathVariable("member-id") Long memberId,
+            @RequestParam(required = false, defaultValue = "1", value = "sort") String sort) {
+        return new BaseResponse<>(memberService.getBrands(memberId, Integer.parseInt(sort)));
     }
 }
