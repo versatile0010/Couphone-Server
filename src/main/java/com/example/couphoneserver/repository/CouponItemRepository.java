f71b1dd69efff1c085cccba07de7cf33390ca690
versatile0010 <siam0294@naver.com>
2023-08-03 18:55:09 +0900

    feat: 만료된 쿠폰은 쿼리에서 제외

--- src/main/java/com/example/couphoneserver/repository/CouponItemRepository.java
+++ src/main/java/com/example/couphoneserver/repository/CouponItemRepository.java
@@ -21,16 +21,15 @@ public interface CouponItemRepository extends JpaRepository<CouponItem, Long> {
 
     Optional<CouponItem> findById(Long couponId);
 
-    // [Note] 만료일이 아니라 생성일로 정렬한 이유는, 만료일은 생성일+6M 이기 때문에 똑같다고 생각했기 떄문입니다.
-    // 특정 회원이 가지고 있는 쿠폰을 1. 도장 개수 많은 순, 2. 생성시간이 빠른 순으로 조회.
-    @Query("SELECT c FROM CouponItem c WHERE c.member.id = :memberId ORDER BY c.stampCount DESC, c.createdDate ASC")
+    // 특정 회원이 가지고 있는 유효한 쿠폰을 1. 도장 개수 많은 순, 2. 생성시간이 빠른 순으로 조회.
+    @Query("SELECT c FROM CouponItem c WHERE c.member.id = :memberId AND c.status <> 'EXPIRED' ORDER BY c.stampCount DESC, c.createdDate ASC")
     List<CouponItem> findByMemberIdOrderByStampCountAndCreatedDate(@Param("memberId") Long memberId);
 
-    // 특정 회원이 가지고 있는 쿠폰을 1. 생성시간이 빠른 순, 2. 도장 개수가 많은 순으로 조회.
-    @Query("SELECT c FROM CouponItem c WHERE c.member.id = :memberId ORDER BY c.createdDate, c.stampCount DESC")
+    // 특정 회원이 가지고 있는 유효한 쿠폰을 1. 생성시간이 빠른 순, 2. 도장 개수가 많은 순으로 조회.
+    @Query("SELECT c FROM CouponItem c WHERE c.member.id = :memberId AND c.status <> 'EXPIRED' ORDER BY c.createdDate, c.stampCount DESC")
     List<CouponItem> findByMemberIdOrderByCreatedDateAndStampCount(@Param("memberId") Long memberId);
 
-    // 특정 회원이 가지고 있는 쿠폰을 브랜드 이름으로 조회
-    @Query("SELECT c FROM CouponItem c WHERE c.member.id = :memberId ORDER BY c.brand.name")
+    // 특정 회원이 가지고 있는 유효한 쿠폰을 브랜드 이름으로 조회
+    @Query("SELECT c FROM CouponItem c WHERE c.member.id = :memberId AND c.status <> 'EXPIRED' ORDER BY c.brand.name")
     List<CouponItem> findByMemberIdOrderByBrandName(@Param("memberId") Long memberId);
 }
