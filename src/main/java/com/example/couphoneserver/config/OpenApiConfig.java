c00c0083c860c0423e7f0cfe9a951fbc526128cc
limsubinn <mymyx02@naver.com>
2023-08-03 10:50:29 +0900

    docs: API title 수정

--- src/main/java/com/example/couphoneserver/config/OpenApiConfig.java
+++ src/main/java/com/example/couphoneserver/config/OpenApiConfig.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "Couphone API"))
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
