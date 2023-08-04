7e6b13afe2525e9d9883a8a7eeeb7aac86d38fd3
versatile0010 <siam0294@naver.com>
2023-08-04 23:45:34 +0900

    feat: 토큰 검증 url 세팅 - "/auth", "/brand", "/users" 에 대해서는 토큰 검증 - "/auth/login" 에 대해서는 토큰 검증 x

--- src/main/java/com/example/couphoneserver/config/WebConfig.java
+++ src/main/java/com/example/couphoneserver/config/WebConfig.java
@@ -21,7 +21,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtAuthenticationInterceptor)
                 .order(1)
-                .addPathPatterns("/auth", "/brands");
+                .addPathPatterns("/auth", "/brands", "/users")
+                .excludePathPatterns("/auth/login");
     }
 
     @Override
